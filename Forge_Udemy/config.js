67f6bf7f404f45438aac58e5d691b5cf1f507478
ossama <ossama@DESKTOP-JIEOPEM>
2023-06-09 07:39:47 +0300

    Add project files.

---
+++ Forge_Udemy/config.js
@@ -0,0 +1,10 @@
+module.exports={
+    credentials:{
+        client_id:"41UCIoqAWFAyXkJtXZueul4m44tTSedu",
+        client_secret:"kzAj2AIiVHJ1pKj0"
+    },
+    scopes:{
+        internal: ['bucket:create','bucket:read','data:read','data:create','data:write'],
+        public:['viewables:read']
+    }
+}
